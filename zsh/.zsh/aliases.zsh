d5cb521bd906e73f38c9fd6ae8ce697599c0bee2
Dschonas <jonasneumair@gmail.com>
2017-10-22 12:31:18 +0200

    added basic zsh config

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,27 @@
+# ALIAS
+alias install='sudo apt install'
+alias nano='sudo nano'
+alias db='~/bin/dropbox.py'
+alias pietsmiet='python ~/Documents/python/pietsmietUploadplan.py'
+alias off='sudo shutdown now'
+alias reboot='sudo shutdown -r now'
+
+alias nvidia-settings='sudo optirun nvidia-settings -c :8'
+alias nvstat='sudo cat /proc/acpi/bbswitch'
+
+# GIT
+alias g='git'
+alias gp='git push'
+alias ga='git add'
+alias gc='git commmit -m'
+alias git-ll='git log --graph --decorate --stat --oneline'
+
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+alias zshconfig="subl ~/.zshrc"
+alias ohmyzsh="subl ~/.oh-my-zsh"
