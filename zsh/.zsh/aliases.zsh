155422899f02984672c8f74d0bb0fd074320ea3a
dschonas <jonasneumair@gmail.com>
2017-11-18 15:22:03 +0100

    Add changes to aliases and add sublime settings

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,20 +1,38 @@
-# ALIAS
+# SYSTEM ALIAS
 alias install='sudo apt install'
 alias nano='sudo nano'
-alias db='~/bin/dropbox.py'
-alias pietsmiet='python ~/Documents/python/pietsmietUploadplan.py'
 alias off='sudo shutdown now'
 alias reboot='sudo shutdown -r now'
-
 alias nvidia-settings='sudo optirun nvidia-settings -c :8'
 alias nvstat='sudo cat /proc/acpi/bbswitch'
 
+# SCRIPTS
+alias db='~/bin/dropbox.py'
+alias pietsmiet='python ~/Documents/python/pietsmietUploadplan.py'
+
+
 # GIT
 alias g='git'
-alias gp='git push'
-alias ga='git add'
-alias gc='git commmit -m'
-alias git-ll='git log --graph --decorate --stat --oneline'
+alias gs='g status'
+alias gp='g push'
+alias ga='g add'
+alias gc='g commit'
+alias gcm='gc -m'
+alias git-ll='g log --graph --decorate --stat --oneline'
+
+alias gconf='git config'
+alias lgconf='gconf --list'
+alias localname='gconf --local user.name'
+alias localmail='gconf --local user.email'
+alias globname='gconf --global user.name'
+alias globalmail='gconf --global user.email'
+
+# GIT FLOW
+alias gf='git flow'
+alias gfi='git flow init'
+alias gfsf='gf feature start'
+alias gfff='gf feature finish'
+alias gfpf='gf feature publish'
 
 
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
